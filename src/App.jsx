79788d11c116b087f20324412ea453b098a9f24b
nipon pal <niponpal52@gmail.com>
2026-03-18 21:51:36 +0600

    update features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Features from "./components/Features/Features";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Features></Features>
       <State></State>
       <Footer></Footer>
     </>
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Features = () => {
   return (
     <div>
-      <p>features section here</p>
+      <h2 className="capitalize text-2xl text center border-b">
+        Your choisen user
+      </h2>
     </div>
   );
 };
