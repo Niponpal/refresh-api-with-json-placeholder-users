76874013629349b256f0779742869be28324a247
nipon pal <niponpal52@gmail.com>
2026-03-18 21:42:45 +0600

    refactor footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import State from "./components/State/State";
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <State></State>
+      <Footer></Footer>
     </>
   );
 }
