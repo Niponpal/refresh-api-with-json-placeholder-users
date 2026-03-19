19d91606c7984880834cfe9308563e7b5106e5cc
nipon pal <niponpal52@gmail.com>
2026-03-19 22:13:47 +0600

    refactor contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,11 @@ const Contact = () => {
   return (
     <div>
       <p>this is contact page</p>
+      <h2>
+        Here is the contact that i use to contact with the team of developers.
+        and they
+      </h2>
+      <span>can be</span>
     </div>
   );
 };
