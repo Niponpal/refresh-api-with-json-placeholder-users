2029c52c1ab50b9de38e493fd7c2dd1822167386
nipon pal <niponpal52@gmail.com>
2026-03-22 21:30:27 +0600

    change contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <p>this is contact edited</p>
+      <p>this is contact edited change</p>
       <h2>
         Here is the contact that i use to contact with the team of developers.
         and they
