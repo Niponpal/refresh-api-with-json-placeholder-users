ca0a4a7f2fdaa7d857402d251edbaab01940cd5e
nipon pal <niponpal52@gmail.com>
2026-03-23 22:05:04 +0600

    updated again fearures page

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -4,7 +4,7 @@ const Features = () => {
   return (
     <div>
       <h2 className="capitalize text-2xl text center border-b">
-        Your choisen user edited
+        Your choisen user edited updated again
       </h2>
     </div>
   );
